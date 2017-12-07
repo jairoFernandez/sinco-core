2fde60c68874a01327dd4788532598b4d3275b08
jairofg12 <jairofg12@outlook.com>
2017-12-07 10:33:10 -0500

    add docs

--- src/sample.component.ts
+++ src/sample.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sample-component',
-  template: `<h1>Sample component cambio en test</h1>`
+  template: `<h1>Sample component cambio en test {{prueba}}</h1>`
 })
 export class SampleComponent {
-
+  prueba: string = "Jairo";
   constructor() {
   }
 
